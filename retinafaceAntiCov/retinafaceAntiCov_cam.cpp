4d016ce923deb32f2e88f4d87eaf8e149edf6fc3
BrightRo99 <brightro99@gmail.com>
2021-01-28 18:09:28 +0900

    Modify retinafaceAntiCov_cam.cpp

--- retinafaceAntiCov/retinafaceAntiCov_cam.cpp
+++ retinafaceAntiCov/retinafaceAntiCov_cam.cpp
@@ -524,7 +524,7 @@ int main(int argc, char** argv) {
     delete[] trtModelStream;
 
     // cv::Mat img = cv::imread("test.jpg");
-    VideoCapture cap(0);
+    cv2::VideoCapture cap(0);
     if (!cap.isOpened())
     {
 	    std::cout << "Can't open the camera" << std:endl;
